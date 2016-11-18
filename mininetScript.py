573549583e381c002c1e0555c6a587df6b2e63c7
MrinalAich <cs16mtech11009@iith.ac.in>
2016-11-19 04:07:53 +0530

    Update mininetScript.py

--- mininetScript.py
+++ mininetScript.py
@@ -31,8 +31,6 @@ def simpleTest():
     print "Dumping host connections"
     dumpNodeConnections(net.hosts)
     CLI(net)
-    #print "Testing network connectivity"
-    #net.pingAll()
     net.stop()
 
 if __name__ == '__main__':
